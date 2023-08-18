708e191a98dc0ca2a8a1853ec565c56998c1e714
FajarSha <fajaroffice.it@gmail.com>
2023-08-18 12:03:53 +0500

    Initial commit

---
+++ app/src/main/java/com/example/housedesign/Modal/ResourceData.java
@@ -0,0 +1,132 @@
+package com.example.housedesign.Modal;
+
+import android.content.Context;
+
+import com.example.housedesign.R;
+
+import java.util.ArrayList;
+
+public class ResourceData {
+    Context context;
+
+    public ResourceData(Context context) {
+        this.context = context;
+    }
+
+    ArrayList<HouseModal> arrayList;
+
+    public ArrayList<HouseModal> getResource(int value) {
+        if (value == 1) {
+            twoDDatatValue();
+        } else if (value == 2) {
+            threeDDatatValue();
+        } else if (value == 3) {
+            frontDatatValue();
+        } else if (value == 4) {
+            bedroomDatatValue();
+          
+        }else if (value == 5) {
+            kitchenDatatValue();
+        }else if (value == 6) {
+            bathroomDatatValue();
+        }else if (value == 7) {
+            loungeDatatValue();
+        }
+        return arrayList;
+
+    }
+
+    private void loungeDatatValue() {
+        arrayList=new ArrayList<>();
+        arrayList.add(new HouseModal(context.getString(R.string.lounge), null, R.drawable.temp_3d_l1));
+        arrayList.add(new HouseModal(context.getString(R.string.lounge), null, R.drawable.temp_3d_l2));
+        arrayList.add(new HouseModal(context.getString(R.string.lounge), null, R.drawable.temp_3d_l3));
+        arrayList.add(new HouseModal(context.getString(R.string.lounge), null, R.drawable.temp_3d_l4));
+        arrayList.add(new HouseModal(context.getString(R.string.lounge), null, R.drawable.temp_3d_l5));
+        arrayList.add(new HouseModal(context.getString(R.string.lounge), null, R.drawable.temp_3d_l6));
+        arrayList.add(new HouseModal(context.getString(R.string.lounge), null, R.drawable.temp_3d_l7));
+        arrayList.add(new HouseModal(context.getString(R.string.lounge), null, R.drawable.temp_3d_l8));
+        arrayList.add(new HouseModal(context.getString(R.string.lounge), null, R.drawable.temp_3d_l9));
+        arrayList.add(new HouseModal(context.getString(R.string.lounge), null, R.drawable.temp_3d_l10));
+    }
+
+    private void bathroomDatatValue() {
+        arrayList=new ArrayList<>();
+        arrayList.add(new HouseModal(context.getString(R.string.bathroom), null, R.drawable.temp_3d_bb1));
+        arrayList.add(new HouseModal(context.getString(R.string.bathroom), null, R.drawable.temp_3d_bb2));
+        arrayList.add(new HouseModal(context.getString(R.string.bathroom), null, R.drawable.temp_3d_bb3));
+        arrayList.add(new HouseModal(context.getString(R.string.bathroom), null, R.drawable.temp_3d_bb4));
+        arrayList.add(new HouseModal(context.getString(R.string.bathroom), null, R.drawable.temp_3d_bb5));
+        arrayList.add(new HouseModal(context.getString(R.string.bathroom), null, R.drawable.temp_3d_bb6));
+        arrayList.add(new HouseModal(context.getString(R.string.bathroom), null, R.drawable.temp_3d_bb7));
+        arrayList.add(new HouseModal(context.getString(R.string.bathroom), null, R.drawable.temp_3d_bb8));
+        arrayList.add(new HouseModal(context.getString(R.string.bathroom), null, R.drawable.temp_3d_bb9));
+        arrayList.add(new HouseModal(context.getString(R.string.bathroom), null, R.drawable.temp_3d_bb10));
+    }
+
+    private void kitchenDatatValue() {
+        arrayList=new ArrayList<>();
+        arrayList.add(new HouseModal(context.getString(R.string.kitchen), null, R.drawable.temp_3d_k1));
+        arrayList.add(new HouseModal(context.getString(R.string.kitchen), null, R.drawable.temp_3d_k2));
+        arrayList.add(new HouseModal(context.getString(R.string.kitchen), null, R.drawable.temp_3d_k3));
+        arrayList.add(new HouseModal(context.getString(R.string.kitchen), null, R.drawable.temp_3d_k4));
+        arrayList.add(new HouseModal(context.getString(R.string.kitchen), null, R.drawable.temp_3d_k5));
+        arrayList.add(new HouseModal(context.getString(R.string.kitchen), null, R.drawable.temp_3d_k6));
+        arrayList.add(new HouseModal(context.getString(R.string.kitchen), null, R.drawable.temp_3d_k7));
+        arrayList.add(new HouseModal(context.getString(R.string.kitchen), null, R.drawable.temp_3d_k8));
+        arrayList.add(new HouseModal(context.getString(R.string.kitchen), null, R.drawable.temp_3d_k9));
+        arrayList.add(new HouseModal(context.getString(R.string.kitchen), null, R.drawable.temp_3d_k10));
+    }
+
+    private void bedroomDatatValue() {
+        arrayList=new ArrayList<>();
+        arrayList.add(new HouseModal(context.getString(R.string.bedroom), null, R.drawable.temp_3d_b1));
+        arrayList.add(new HouseModal(context.getString(R.string.bedroom), null, R.drawable.temp_3d_b2));
+        arrayList.add(new HouseModal(context.getString(R.string.bedroom), null, R.drawable.temp_3d_b3));
+        arrayList.add(new HouseModal(context.getString(R.string.bedroom), null, R.drawable.temp_3d_b4));
+        arrayList.add(new HouseModal(context.getString(R.string.bedroom), null, R.drawable.temp_3d_b5));
+        arrayList.add(new HouseModal(context.getString(R.string.bedroom), null, R.drawable.temp_3d_b6));
+        arrayList.add(new HouseModal(context.getString(R.string.bedroom), null, R.drawable.temp_3d_b7));
+        arrayList.add(new HouseModal(context.getString(R.string.bedroom), null, R.drawable.temp_3d_b8));
+        arrayList.add(new HouseModal(context.getString(R.string.bedroom), null, R.drawable.temp_3d_b9));
+        arrayList.add(new HouseModal(context.getString(R.string.bedroom), null, R.drawable.temp_3d_b10));
+    }
+
+    private void frontDatatValue() {
+        arrayList=new ArrayList<>();
+        arrayList.add(new HouseModal(context.getString(R.string.front), null, R.drawable.temp_3d_f1));
+        arrayList.add(new HouseModal(context.getString(R.string.front), null, R.drawable.temp_3d_f2));
+        arrayList.add(new HouseModal(context.getString(R.string.front), null, R.drawable.temp_3d_f3));
+        arrayList.add(new HouseModal(context.getString(R.string.front),null, R.drawable.temp_3d_f4));
+        arrayList.add(new HouseModal(context.getString(R.string.front), null, R.drawable.temp_3d_f5));
+        arrayList.add(new HouseModal(context.getString(R.string.front), null, R.drawable.temp_3d_f6));
+        arrayList.add(new HouseModal(context.getString(R.string.front), null, R.drawable.temp_3d_f7));
+        arrayList.add(new HouseModal(context.getString(R.string.front), null, R.drawable.temp_3d_f8));
+        arrayList.add(new HouseModal(context.getString(R.string.front), null, R.drawable.temp_3d_f9));
+        arrayList.add(new HouseModal(context.getString(R.string.front), null, R.drawable.temp_3d_f10));
+    }
+
+    private void threeDDatatValue() {
+        arrayList=new ArrayList<>();
+        arrayList.add(new HouseModal(context.getString(R.string.threed), context.getString(R.string.front), R.drawable.front));
+        arrayList.add(new HouseModal(context.getString(R.string.threed), context.getString(R.string.bedroom), R.drawable.bedrroom));
+        arrayList.add(new HouseModal(context.getString(R.string.threed), context.getString(R.string.kitchen), R.drawable.kitchen));
+        arrayList.add(new HouseModal(context.getString(R.string.threed), context.getString(R.string.bathroom), R.drawable.bathroom));
+        arrayList.add(new HouseModal(context.getString(R.string.threed), context.getString(R.string.lounge), R.drawable.lounge));
+    }
+
+    private void twoDDatatValue() {
+        arrayList=new ArrayList<>();
+        arrayList.add(new HouseModal(context.getString(R.string.twod), null, R.drawable.temp_2d_1));
+        arrayList.add(new HouseModal(context.getString(R.string.twod), null, R.drawable.temp_2d_2));
+        arrayList.add(new HouseModal(context.getString(R.string.twod), null, R.drawable.temp_2d_3));
+        arrayList.add(new HouseModal(context.getString(R.string.twod), null, R.drawable.temp_2d_4));
+        arrayList.add(new HouseModal(context.getString(R.string.twod), null, R.drawable.temp_2d_5));
+        arrayList.add(new HouseModal(context.getString(R.string.twod), null, R.drawable.temp_2d_6));
+        arrayList.add(new HouseModal(context.getString(R.string.twod), null, R.drawable.temp_2d_7));
+        arrayList.add(new HouseModal(context.getString(R.string.twod), null, R.drawable.temp_2d_8));
+        arrayList.add(new HouseModal(context.getString(R.string.twod), null, R.drawable.temp_2d_9));
+        arrayList.add(new HouseModal(context.getString(R.string.twod), null, R.drawable.temp_2d_10));
+
+    }
+}
---
+++ app/src/main/java/com/example/housedesign/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.housedesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+ImageView getStart_btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        initView();
+        getStart_btn.setOnClickListener(view -> {
+            startActivity(new Intent(this,MainActivity.class));
+        });
+    }
+
+    private void initView() {
+        getStart_btn=findViewById(R.id.getStart_btn);
+    }
+}
